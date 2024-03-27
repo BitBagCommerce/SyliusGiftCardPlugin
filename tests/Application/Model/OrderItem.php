7c419b829ed04433b7100bce7fc6d5634ea7f67f
Marek Rzytki <rzytki7@gmail.com>
2024-03-27 14:47:11 +0100

    Remove equals function override

--- tests/Application/Model/OrderItem.php
+++ tests/Application/Model/OrderItem.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Tests\Setono\SyliusGiftCardPlugin\Application\Model;
 
 use Doctrine\ORM\Mapping as ORM;
-use Setono\SyliusGiftCardPlugin\Model\OrderItemTrait;
 use Setono\SyliusGiftCardPlugin\Model\ProductInterface;
 use Sylius\Component\Core\Model\OrderItem as BaseOrderItem;
 
@@ -17,5 +16,4 @@ use Sylius\Component\Core\Model\OrderItem as BaseOrderItem;
  */
 class OrderItem extends BaseOrderItem
 {
-    use OrderItemTrait;
 }
